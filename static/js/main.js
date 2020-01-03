f26ad75b08ee481fe675395eb1abc59b95827c7a
Luis <luis@luisem.co>
2020-01-03 10:27:59 -0300

    Web

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed-title', {
+  strings: ["Backtest your strategies.", "Broker connections simplified.", "Interactive Brokers", "TD Ameritrade", "E-Trade", "MetaTrader", "Stock Analysis", "Map Analysis"],
+  typeSpeed: 100,
+  startDelay: 1000,
+  loop: true,
+  cursorChar: null,
+  loopCount: Infinity
+});
